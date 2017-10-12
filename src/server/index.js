a548d28592eed733a201edf1c4fd155a8a0ed816
Vincent Abruzzo <vgabruzzo@gmail.com>
2017-10-12 01:35:33 -0400

    add basic crud

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,25 @@
-const Koa = require("koa");
-const bodyParser = require("koa-bodyparser");
+// external imports
+const Koa = require('koa');
+const r = require('rethinkdbdash');
+const bodyParser = require('koa-bodyparser');
 
+// internal imports
+const indexRoutes = require('./routes/index');
+const quoteRoutes = require('./routes/quotes');
+
+// init server & port
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
+// init db connection pool & make available to app via context prototype.
+// access db pool in middleware via ctx.db - the parameters in our db/model
+// queries is named r, since we're using rethinkdb
+app.context.db = r();
+
+// middlewares
 app.use(bodyParser());
+app.use(indexRoutes.routes());
+app.use(quoteRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
